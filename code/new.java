dabcac613bd09593b5a40bda2531f71c9805b656
Mohit <mohkumar24@publicisgroupe.net>
2022-01-18 18:25:57 +0530

    Edited new.java

--- code/new.java
+++ code/new.java
@@ -2,6 +2,10 @@ class gurpreet
 {
 	public static void main(string Args[])
 	{
-		System.out.println("gurpreet")
+		System.out.println("gurpreet");
+		newDemo("Hello");
 		}
+	public void newDemo(String s){
+		System.out.println(s);
+	}
 }
